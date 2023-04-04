4bc05165e405da653f5bb0675b1324ae91dc452d
Y2FuZXBh <Y2FuZXBh@proton.me>
2023-04-04 11:12:51 -0700

    update

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ _docker.containers.run(
     name="jupyter-kali",
     image="jupyter-kali:latest",
     ports={'8888/tcp': (BIND_IP, BIND_PORT)},
-    command="--restart always",
+    restart_policy={"Name": "always"},
     detach=True
 )
 time.sleep(5)
